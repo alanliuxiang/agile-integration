7bfd72e0c553067252df8c699cdd950c29b1ec16
kylin <kylinsoong.1214@gmail.com>
2018-01-10 22:41:36 +0800

    add new component example

--- camel-dev/src/main/java/org/apache/camel/examples/CamelComponentActiveMQ.java
+++ camel-dev/src/main/java/org/apache/camel/examples/CamelComponentActiveMQ.java
@@ -2,13 +2,11 @@ package org.apache.camel.examples;
 
 import static org.apache.camel.examples.Runner.starter;
 
-public class CamelDataFormatJSON {
+public class CamelComponentActiveMQ {
 
 	public static void main(String[] args) {
 
-		starter("camelContext-json.xml", 1000 * 3);
-		
-		starter("camelContext-json-http.xml", 1000 * 3);
+		starter("camelContext-activemq.xml", 10000);
 	}
 
 }
