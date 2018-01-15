af445d69444b653a430dc256ed3db7d84eea388b
kylin <kylinsoong.1214@gmail.com>
2018-01-15 19:15:56 +0800

    finish the external properties mapping

--- camel-dev/src/main/java/org/apache/camel/examples/processor/DBPropertiesProcessor.java
+++ camel-dev/src/main/java/org/apache/camel/examples/processor/DBPropertiesProcessor.java
@@ -1,14 +1,31 @@
 package org.apache.camel.examples.processor;
 
+import java.util.Map;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
 public class DBPropertiesProcessor implements Processor {
+	
+	private static final String RESTTYPE_DESC = "restype_description";
+	private static final String CATEGORY_DESC = "category_description";
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
-
-		System.out.println(exchange);
+		
+		try {
+			Object restTypeDesc = exchange.getIn().getHeader(RESTTYPE_DESC);
+			Object categoryTypeDesc = exchange.getIn().getHeader(CATEGORY_DESC);
+			@SuppressWarnings("unchecked")
+			Map<String, Object> map = (Map<String, Object>) exchange.getIn().getBody();
+			map.put(RESTTYPE_DESC, restTypeDesc);
+			Object currentDesc = map.get(CATEGORY_DESC);
+			
+			String newDesc = categoryTypeDesc + ": " + currentDesc;
+			map.put(CATEGORY_DESC, newDesc);
+		} catch (Exception e) {
+			throw new RuntimeException("External Properties Mapping Exception, " + e.getMessage());
+		}
 		
 		exchange.setOut(exchange.getIn());
 		
