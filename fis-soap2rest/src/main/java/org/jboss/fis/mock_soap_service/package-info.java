e9d56b4b1b3264c0988eb14c23d205e165592fe7
kylin <kylinsoong.1214@gmail.com>
2018-01-31 19:50:14 +0800

    add soap to rest

---
+++ fis-soap2rest/src/main/java/org/jboss/fis/mock_soap_service/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://jboss.org/fis/mock-soap-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.jboss.fis.mock_soap_service;
